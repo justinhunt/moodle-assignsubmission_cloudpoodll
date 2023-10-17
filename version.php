30440fac0dc190069f3e26a7ad857c00e73a3637
justinhunt <bitwalkerjapan@gmail.com>
2023-10-17 08:30:22 +0000

    version bump

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   =  2023101300;
+$plugin->version   =  2023101700;
 $plugin->requires  = 2015051100; // Requires Moodle version 2.9 or greater
 $plugin->component =  'assignsubmission_cloudpoodll';
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '1.0.37 (Build 2023101300)';
+$plugin->release   = '1.0.38 (Build 2023101700)';
